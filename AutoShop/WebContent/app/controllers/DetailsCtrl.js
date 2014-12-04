fa4b1498d813e2ddfe6399053efed41c4930e671
thomas.gruszka <thomas.gruszka@stud.fh-rosenheim.de>
2014-12-04 16:07:27 +0100

    carousel probieren

--- AutoShop/WebContent/app/controllers/DetailsCtrl.js
+++ AutoShop/WebContent/app/controllers/DetailsCtrl.js
@@ -2,7 +2,19 @@ app.controller('DetailsCtrl', function($scope, $routeParams, SearchResultService
 	
 	$scope.$on('$viewContentLoaded', function () {
 		var carID = $routeParams.id;
+		var carouselImages = [];
 		
 		$scope.car = SearchResultService.getCar(carID);
+		
+		var count = 0;
+		
+		jQuery.each($scope.car, function(i, val) {
+			if(i == "image" || i == "image2" || i == "image3"){
+				carouselImages[count] = { "image" : val};
+				count++;
+			}
+		});
+		
+		$scope.images = carouselImages;
 	});
 });
